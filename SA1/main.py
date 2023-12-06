ceae717ce1c8d4dfb233eb63036558615b6d988d
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-12-06 10:01:11 +0530

    Delete SA1 directory

--- SA1/main.py
+++
@@ -1,9 +0,0 @@
-# Import the Fernet from cryptography
-from cryptography.fernet import Fernet
-
-# Genrate the secrete key
-key = Fernet.generate_key()
-
-# Store / write the key value inside the encryptedKey.key file
-with open("encryptedKey.key", "wb") as encryptedKey:
-    encryptedKey.write(key)
